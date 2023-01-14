7b3eaef7ac8b50bb40180abb18f8235debd47fc6
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2023-01-15 00:33:14 +0530

    Update mod_arithmatic.cpp

--- NUMBER_THEORY/mod_arithmatic.cpp
+++ NUMBER_THEORY/mod_arithmatic.cpp
@@ -4,6 +4,9 @@ ll fact[1000004];
 ll modinv[1000004];
 
 ll power(ll base, ll x){
+    if(x < 0){
+        return 0;
+    }
     ll ans = 1;
     while(x){
         if(x % 2 == 0){
@@ -20,14 +23,18 @@ ll power(ll base, ll x){
 void precomp(){
     modinv[0] = 1;
     fact[0] = 1;
-    for(int i = 1; i <= 1000000; i++){
+    for(ll i = 1; i <= 1000000; i++){
         fact[i] = (fact[i-1]*i)%mod;
         modinv[i] = power(fact[i], mod-2);
     }
 }
 
-ll ncr(int n, int r){
+ll ncr(ll n, ll r){
+    if(n < 0 || r < 0 || r > n){
+        return 0;
+    }
     ll numo = fact[n];
     ll ans = (((numo * modinv[r])%mod)*modinv[n-r]) % mod;
     return ans;
 }
+
